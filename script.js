6b30d1af24c24d998b302a9f8e60d487ddd932c9
akshukla0909 <aakshukla1947@gmail.com>
2023-04-05 10:30:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var change = document.querySelector("h6");
+var changePhoto =document.querySelector("img");
+
+change.addEventListener("click",function(){
+   changePhoto.style.backgroundImage = "url(https://cdn.sanity.io/images/c1chvb1i/production/c552d45249cdd8810da9d34c105a192c2b9ac7db-1200x802.jpg/nike-air-max-90-golf-red-5.jpg?fm=webp)"
+})
